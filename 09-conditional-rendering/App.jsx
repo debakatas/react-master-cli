c769132f7202636c111f85084089d33ce7d0d89a
Nicolás M. Pardo <nicolasm@nikodermus.media>
2020-06-10 20:25:35 -0500

    Update live workshop

--- 09-conditional-rendering/App.jsx
+++ 09-conditional-rendering/App.jsx
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import moon from './img/moon.svg';
 import sun from './img/sun.svg';
 
 import './style.css';
 
-const App = () => (
-    <main className="night day">
-        <label className="toggle" htmlFor="dabox">
-            <input type="checkbox" id="dabox" />
-            <span className="slider" />
-        </label>
-        <img src={sun} alt="Sun" />
-        <img src={moon} alt="Moon" />
+class App extends Component {
+    state = {
+        day: false,
+    };
 
-        <h2>zZzZzZz...</h2>
-    </main>
-);
+    changeTime = () => {
+        this.setState((stateAntesDelCambio) => ({
+            day: !stateAntesDelCambio.day,
+        }));
+    };
+
+    render() {
+        const { day } = this.state;
+
+        return (
+            <main className={day ? 'day' : 'night'}>
+                <label className="toggle" htmlFor="dabox">
+                    <input
+                        type="checkbox"
+                        id="dabox"
+                        onChange={this.changeTime}
+                        checked={day}
+                    />
+                    <span className="slider" />
+                </label>
+
+                <img src={day ? sun : moon} alt="Sun" />
+
+                {!day && <h2>zZzZzZz...</h2>}
+            </main>
+        );
+    }
+}
 
 export default App;
