5c8f8c2f146cc9ba848e0879412e350d97e8e092
Nicolás M. Pardo <nicolasm@nikodermus.media>
2020-06-17 09:29:19 -0500

    fix(11/app.jsx): avoid editing original element

--- 11-list-rendering-shared/components/App.jsx
+++ 11-list-rendering-shared/components/App.jsx
@@ -5,7 +5,7 @@ import movies from '../data/movies';
 import like from '../img/like.svg';
 import dislike from '../img/dislike.svg';
 import reset from '../img/reset.svg';
-/* eslint-disable */
+
 class App extends Component {
     state = {
         movies,
@@ -22,15 +22,21 @@ class App extends Component {
                 // Usamos findIndex para saber que elemento
                 // queremos editar, queremos el indice, un número,
                 // debido a que movies es un Array
-                const movieIndex = prevState.movies.findIndex(movie => movie.name === movieName);
+                const movieIndex = prevState.movies.findIndex(
+                    (movie) => movie.name === movieName
+                );
+                const moviesCopy = [...prevState.movies];
 
                 // Editamos de movies el elemento que encontramos.
-                prevState.movies[movieIndex].status = likeStatus;
+                moviesCopy[movieIndex] = {
+                    ...moviesCopy[movieIndex],
+                    status: likeStatus,
+                };
 
                 // Devolvemos uun objeto con la propiedad "movies"
                 // que es lo que queremos editar
                 return {
-                    movies: prevState.movies
+                    movies: moviesCopy,
                 };
             }
         );
@@ -41,25 +47,24 @@ class App extends Component {
         this.setState(
             // Usamos la funcion para pasar por todos los elementos del estado actual
             // porque puede haber elementos eliminados
-            (prevState) => {
+            (prevState) =>
                 // Devolvemos un objeto con la propiedead "movies"
                 // que es la que queremos editar
-                return {
+                ({
                     // Creamos la propiedad movies
                     // iterando sobre las movies actuales
-                    movies: prevState.movies.map(movie => (
+                    movies: prevState.movies.map((movie) =>
                         // Usamos el spread operator para
                         // mantener todas las propiedades de cada pelicula
                         // debido a que son objetos
-                        {
+                        ({
                             ...movie,
                             // Cambiamos la propiedad que queremos
                             // por el valor que recibimos como parametro
-                            status: likeStatus
-                        }
-                    ))
-                };
-            }
+                            status: likeStatus,
+                        })
+                    ),
+                })
         );
     };
 
@@ -67,7 +72,7 @@ class App extends Component {
         this.setState({
             // Devolvemos las peliculas al estado inicial,
             // que es la data que viene del archivo.
-            movies: movies
+            movies,
         });
     };
 
@@ -78,12 +83,14 @@ class App extends Component {
             (prevState) => {
                 // Usamos filter para eliminar del array el elemento que tenga el mismo name
                 // del elemento que recibimos por parametros
-                const movies = prevState.movies.filter(movie => movie.name !== movieName);
+                const newMovies = prevState.movies.filter(
+                    (movie) => movie.name !== movieName
+                );
 
                 // Devolvemos un objeto con la propiedead "movies"
                 // que es la que queremos editar
                 return {
-                    movies: movies
+                    movies: newMovies,
                 };
             }
         );
@@ -91,102 +98,95 @@ class App extends Component {
 
     render() {
         // Usamos filter para crear un array único por cada status ""|"like"|"dislike"
-        const likedMovies = this.state.movies.filter(movie => movie.status === "like");
-        const dislikedMovies = this.state.movies.filter(movie => movie.status === "dislike");
-        const neutralMovies = this.state.movies.filter(movie => movie.status === "");
-
+        const likedMovies = this.state.movies.filter(
+            (movie) => movie.status === 'like'
+        );
+        const dislikedMovies = this.state.movies.filter(
+            (movie) => movie.status === 'dislike'
+        );
+        const neutralMovies = this.state.movies.filter(
+            (movie) => movie.status === ''
+        );
 
         return (
             <main>
                 <header>
                     <h1>Time Travel Movies</h1>
-                    <button type="button">
-                        <img
-                            src={like}
-                            alt="Vote up all"
-                            onClick={() => this.changeAllMovieStatuses('like')}
-                        />
+                    <button
+                        type="button"
+                        onClick={() => this.changeAllMovieStatuses('like')}
+                    >
+                        <img src={like} alt="Vote up all" />
                     </button>
-                    <button type="button">
-                        <img
-                            src={reset}
-                            alt="Reset score"
-                            onClick={this.resetMovies} />
+                    <button type="button" onClick={this.resetMovies}>
+                        <img src={reset} alt="Reset score" />
                     </button>
-                    <button type="button">
-                        <img
-                            src={dislike}
-                            alt="Vote down all"
-                            onClick={() => this.changeAllMovieStatuses('like')} />
+                    <button
+                        type="button"
+                        onClick={() => this.changeAllMovieStatuses('dislike')}
+                    >
+                        <img src={dislike} alt="Vote down all" />
                     </button>
                 </header>
 
                 {/* Solo renderizamos si hay algún elemento en el array */}
-                {
-                    likedMovies.length > 0 && (
-                        <>
-                            <h2>
-                                <span>Liked</span>
-                            </h2>
+                {likedMovies.length > 0 && (
+                    <>
+                        <h2>
+                            <span>Liked</span>
+                        </h2>
 
-                            <ul>
-                                {likedMovies.map((movie) => (
-                                    <Movie
-                                        key={movie.name}
-                                        // Usamos el spread operator
-                                        // para pasar todas las propiedades
-                                        // de movie como props
-                                        {...movie}
-                                        changeMovieStatus={this.changeMovieStatus}
-                                        deleteMovie={this.deleteMovie}
-                                    />
-                                ))}
-                            </ul>
-                            <hr />
-                        </>
-                    )
-                }
+                        <ul>
+                            {likedMovies.map((movie) => (
+                                <Movie
+                                    key={movie.name}
+                                    // Usamos el spread operator
+                                    // para pasar todas las propiedades
+                                    // de movie como props
+                                    {...movie}
+                                    changeMovieStatus={this.changeMovieStatus}
+                                    deleteMovie={this.deleteMovie}
+                                />
+                            ))}
+                        </ul>
+                        <hr />
+                    </>
+                )}
 
-                {
-                    neutralMovies.length > 0 && (
-                        <>
-                            <ul>
-                                {neutralMovies.map((movie) => (
-                                    <Movie
-                                        key={movie.name}
-                                        {...movie}
-                                        changeMovieStatus={this.changeMovieStatus}
-                                        deleteMovie={this.deleteMovie}
+                {neutralMovies.length > 0 && (
+                    <>
+                        <ul>
+                            {neutralMovies.map((movie) => (
+                                <Movie
+                                    key={movie.name}
+                                    {...movie}
+                                    changeMovieStatus={this.changeMovieStatus}
+                                    deleteMovie={this.deleteMovie}
+                                />
+                            ))}
+                        </ul>
+                        <hr />
+                    </>
+                )}
 
-                                    />
-                                ))}
-                            </ul>
-                            <hr />
-                        </>
-                    )
-                }
-
-                {
-                    dislikedMovies.length > 0 && (
-                        <>
-                            <h2>
-                                <span>Disliked</span>
-                            </h2>
-                            <ul>
-                                {dislikedMovies.map((movie) => (
-                                    <Movie
-                                        key={movie.name}
-                                        {...movie}
-                                        changeMovieStatus={this.changeMovieStatus}
-                                        deleteMovie={this.deleteMovie}
-
-                                    />
-                                ))}
-                            </ul>
-                            <hr />
-                        </>
-                    )
-                }
+                {dislikedMovies.length > 0 && (
+                    <>
+                        <h2>
+                            <span>Disliked</span>
+                        </h2>
+                        <ul>
+                            {dislikedMovies.map((movie) => (
+                                <Movie
+                                    key={movie.name}
+                                    {...movie}
+                                    changeMovieStatus={this.changeMovieStatus}
+                                    deleteMovie={this.deleteMovie}
+                                />
+                            ))}
+                        </ul>
+                        <hr />
+                    </>
+                )}
             </main>
         );
     }
