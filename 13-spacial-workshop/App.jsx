e38c61521eb427be64d7a0563a5405c59384aa60
Nicolás M. Pardo <nicolasm@nikodermus.media>
2020-06-19 20:21:20 -0500

    Update live workshop

--- 13-spacial-workshop/App.jsx
+++ 13-spacial-workshop/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     Navbar,
     Container,
@@ -13,43 +13,75 @@ import {
 import './style.css';
 
 const NASA_API = 'https://api.nasa.gov/';
-const API_KEY = 'DEMO_KEY';
+const API_KEY = 'api_key=rOI9SGtuJbeH61dcvjxZz8eXURdOvTzGpcTJTKL7';
 
-const App = () => (
-    <>
-        <Navbar bg="dark" className="justify-content-between">
-            <Navbar.Brand className="display-6 text-white" as="h2">
-                API Espacial
-            </Navbar.Brand>
-            <Form inline>
-                <FormControl
-                    type="text"
-                    placeholder="Search"
-                    className="mr-sm-2"
-                />
-                <Button variant="outline-light">Search</Button>
-            </Form>
-        </Navbar>
-        <Container as="main">
-            <Row className="justify-content-md-center my-4">
-                <Col>
-                    <Jumbotron>
-                        <h1>
-                            ¿Qué puedes crear <Badge variant="dark">hoy</Badge>?
-                        </h1>
-                        <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing
-                            elit. Quas atque qui officiis ab rerum doloremque
-                            numquam nobis modi! Ad voluptatibus consequuntur
-                            eius sunt magnam incidunt quae laborum officia in
-                            nesciunt?
-                        </p>
-                        <Button>Cualquier cosa!</Button>
-                    </Jumbotron>
-                </Col>
-            </Row>
-        </Container>
-    </>
-);
+class App extends Component {
+    state = {
+        nombre: 'API Espacial',
+        photos: [],
+    };
+
+    componentDidMount() {
+        fetch(
+            `${NASA_API}mars-photos/api/v1/rovers/curiosity/photos?sol=10&${API_KEY}`
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({ photos: data.photos });
+            });
+    }
+
+    cambiar = () => {
+        this.setState({ nombre: 'Otra cosa' });
+    };
+
+    render() {
+        return (
+            <>
+                <Navbar bg="dark" className="justify-content-between">
+                    <Navbar.Brand className="display-6 text-white" as="h2">
+                        {this.state.nombre}
+                    </Navbar.Brand>
+                    <Form inline>
+                        <FormControl
+                            type="text"
+                            placeholder="Search"
+                            className="mr-sm-2"
+                        />
+                        <Button variant="outline-light">Search</Button>
+                    </Form>
+                </Navbar>
+                <Container as="main">
+                    <Row className="justify-content-md-center my-4">
+                        <Col>
+                            <Jumbotron>
+                                <h1>
+                                    ¿Qué puedes crear{' '}
+                                    <Badge variant="dark">hoy</Badge>?
+                                </h1>
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur
+                                    adipisicing elit. Quas atque qui officiis ab
+                                    rerum doloremque numquam nobis modi! Ad
+                                    voluptatibus consequuntur eius sunt magnam
+                                    incidunt quae laborum officia in nesciunt?
+                                </p>
+                                <Button onClick={this.cambiar}>
+                                    Cualquier cosa!
+                                </Button>
+                            </Jumbotron>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        {this.state.photos.map((photo) => (
+                            <img src={photo.img_src} alt="" />
+                        ))}
+                    </Row>
+                </Container>
+            </>
+        );
+    }
+}
 
 export default App;
